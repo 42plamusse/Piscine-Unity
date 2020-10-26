0963770677c5df32610117853b3d4490d297d50a
Pierre Lamusse <plamusse42@gmail.com>
2020-10-26 09:01:35 +0100

    [add] d00-ex02 - hole dectection

---
+++ d00/Assets/Club.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Club : MonoBehaviour
+{
+    public Ball ball;
+    public float minForce, maxForce;
+
+    public float putForce;
+
+    bool charging = false;
+    private void Start()
+    {
+        putForce = 0f;
+        //ball = GameObject.FindGameObjectsWithTag("Ball").GetComponent<Ball>();
+
+    }
+    private void Update()
+    {
+        if (!ball.shot && Input.GetKey(KeyCode.Space))
+        {
+            putForce = Mathf.Clamp(putForce + 20 * Time.deltaTime, minForce, maxForce);
+            charging = true;
+            if(putForce < maxForce)
+            {
+                transform.Translate(-ball.direction * Time.deltaTime);
+            }
+        } else
+        {
+            charging = false;
+        }
+        if (!charging && putForce != 0f)
+        {
+            this.GetComponent<Renderer>().enabled = false;
+            ball.ShootBall(putForce);
+            putForce = 0f;
+        }
+    }
+
+    public void ChangePos()
+    {
+        // rotate ?
+        transform.position = new Vector3(
+            transform.position.x,
+            ball.transform.position.y + 0.5f + (-ball.direction.y * .2f),
+            transform.position.z);
+        this.GetComponent<Renderer>().enabled = true;
+    }
+}
---
+++ d00/Assets/ex02/Scripts/Ball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Club club;
+    public Transform hole;
+    public float inertia;
+
+    public bool shot = false;
+    public Vector3 direction = Vector3.up;
+
+
+    float force;
+    float timeSinceShot;
+    float ballSpeed;
+
+    private void Update()
+    {
+        if (transform.position.y >= 4.7 || transform.position.y <= -4.7 ){
+            transform.Translate(-direction * ballSpeed);
+            direction *= -1;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (shot && force != 0)
+        {
+            timeSinceShot += Time.fixedDeltaTime;
+            ballSpeed = force / inertia / timeSinceShot * Time.fixedDeltaTime;
+            if (ballSpeed > 0.01f)
+            {
+                transform.Translate(direction * ballSpeed);
+            }
+            else
+            {
+                timeSinceShot = 0;
+                shot = false;
+                if (transform.position.y < hole.position.y) direction = Vector3.up;
+                else direction = Vector3.down;
+                force = 0f;
+                club.ChangePos(); 
+            }
+        }
+    }
+
+    public void ShootBall(float putForce)
+    {
+        shot = true;
+        force = putForce;
+    }
+}
