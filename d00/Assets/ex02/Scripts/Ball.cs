ff68afd26393ff747d233e06011e68e06eb3c915
Pierre Lamusse <plamusse42@gmail.com>
2020-10-26 13:24:09 +0100

    [add] - ex02

--- d00/Assets/ex02/Scripts/Ball.cs
+++ d00/Assets/ex02/Scripts/Ball.cs
@@ -10,21 +10,31 @@ public class Ball : MonoBehaviour
 
     public bool shot = false;
     public Vector3 direction = Vector3.up;
+    public bool isGameOver = false;
 
 
     float force;
     float timeSinceShot;
     float ballSpeed;
+    int score = -15;
 
+    private void Start()
+    {
+        print("Score: " + score);
+    }
     private void Update()
     {
+        if (isGameOver) return;
         if (transform.position.y >= 4.7 || transform.position.y <= -4.7 ){
             transform.Translate(-direction * ballSpeed);
             direction *= -1;
         }
+        IsInHole();
     }
+
     private void FixedUpdate()
     {
+        if (isGameOver) return;
         if (shot && force != 0)
         {
             timeSinceShot += Time.fixedDeltaTime;
@@ -40,7 +50,24 @@ public class Ball : MonoBehaviour
                 if (transform.position.y < hole.position.y) direction = Vector3.up;
                 else direction = Vector3.down;
                 force = 0f;
-                club.ChangePos(); 
+                club.ChangePos();
+                score += 5;
+                print("Score: " + score);
+
+            }
+        }
+    }
+
+    void IsInHole()
+    {
+        if(transform.position.y > 3.8 && transform.position.y < 4.2)
+        {
+            if(ballSpeed < 0.02f)
+            {
+                if (score > 0) print("You Lose !");
+                else print("You win !");
+                print("Final score: " + score);
+                isGameOver = true;
             }
         }
     }
