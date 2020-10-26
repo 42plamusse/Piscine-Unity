ffcd6a66ce884e770e4391b5ac5429ba680a6893
Pierre Lamusse <plamusse42@gmail.com>
2020-10-26 19:47:12 +0100

    [update] - ex00 to match 42 subject

--- d00/Assets/ex00/Scripts/Balloon.cs
+++ d00/Assets/ex00/Scripts/Balloon.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Balloon : MonoBehaviour
 {
     public float blowForce = .02f;
-    public float losesAir = .01f;
+    public float losesAir = .5f;
     public float maxTimeBlowing = 1.0f;
-    public float minBaloonScale = 0.8f;
+    public float minBaloonScale = 0.2f;
     public float maxBaloonScale = 3f;
 
 
@@ -19,24 +19,19 @@ public class Balloon : MonoBehaviour
     private void Update()
     { 
 
-        if (Input.GetKeyDown("space"))
+        if (!blow && Input.GetKeyDown("space"))
         {
             if (!started) started = true;
             blow = true;
         }
         if (!started) return;
         timeSinceStartInSeconds += Time.deltaTime;
-        if (Input.GetKeyUp("space"))
-        {
-            timeBlowing = 0f;
-            blow = false;
-        }
         if (transform.localScale.x < minBaloonScale ||
             transform.localScale.x > maxBaloonScale)
         {
             print("Balloon life time : " +
                 Mathf.RoundToInt(timeSinceStartInSeconds).ToString() + "s");
-            Destroy(this);
+            Destroy(this.gameObject);
         }
     }
 
@@ -51,6 +46,8 @@ public class Balloon : MonoBehaviour
         }
         else
         {
+            timeBlowing = 0f;
+            blow = false;
             transform.localScale = new Vector3(transform.localScale.x - losesAir,
                transform.localScale.y - losesAir, transform.localScale.z);
         }
