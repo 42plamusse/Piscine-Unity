2443ce8387bbbfe1798128347c3f18460ea49998
Pierre Lamusse <plamusse42@gmail.com>
2020-10-26 19:14:56 +0100

    [add] - ex03

---
+++ d00/Assets/ex03/Scripts/Bird.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed;
+    public float pipeSpeed = 1f;
+    public float jumpForce;
+    public float addJumpTime;
+
+    float jumpTime;
+    int score = 0;
+    float timeAlive = 0f;
+    bool gameOver = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAlive += Time.deltaTime;
+        if (transform.position.y < -2.9f) gameOver = true;
+
+        if (gameOver) return;
+
+        if (transform.position.y < 4f &&
+            jumpTime <=0 &&
+            Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpTime = addJumpTime;
+        }
+        else if(jumpTime > 0)
+        {
+            jumpTime -= Time.deltaTime;
+            transform.Translate(Vector3.up * jumpForce * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.down * speed *  Time.deltaTime);
+        }
+    }
+    public void GameOver()
+    {
+        gameOver = true;
+        print("Score: " + score);
+        print("Time: " + Mathf.RoundToInt(timeAlive));
+    }
+    public void UpdateScore()
+    {
+        score += 5;
+        pipeSpeed += 0.5f;
+    }
+}
---
+++ d00/Assets/ex03/Scripts/Pipe.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public Pipe otherPipe;
+    public Bird bird;
+    public float speed;
+    public Vector3 initialPos;
+    public Vector3 offset;
+
+    float collesionStartx;
+    bool birdInside;
+    bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = initialPos + offset;
+        collesionStartx = 1.1f;
+        birdInside = false;
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver) return;
+        transform.Translate(Vector3.left * bird.pipeSpeed * Time.deltaTime);
+        if (transform.position.x < -initialPos.x) transform.position = initialPos;
+        if (transform.position.x < collesionStartx &&
+            transform.position.x > -collesionStartx)
+        {
+            birdInside = true;
+            float birdY = bird.gameObject.transform.position.y;
+            if (birdY >= 1.76 || birdY <= -1.12)
+            {
+                gameOver = true;
+                bird.GameOver();
+                otherPipe.GameOver();
+            }
+        }
+        if (birdInside && transform.position.x < -collesionStartx)
+        {
+            bird.UpdateScore();
+            birdInside = false;
+
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+    }
+}
