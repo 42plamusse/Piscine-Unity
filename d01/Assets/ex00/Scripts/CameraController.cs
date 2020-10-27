5dc3131b1f28ecd1429cdb8a1a1e56fd55d0a865
Pierre Lamusse <plamusse42@gmail.com>
2020-10-27 09:03:21 +0100

    [d01] - ex00 - jump and camera control

---
+++ d01/Assets/ex00/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerScript_ex00 followedPlayer;
+
+    void Update()
+    {
+        Vector3 newPos = followedPlayer.transform.position;
+        transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+    }
+
+    public void changeFollowedPlayer(PlayerScript_ex00 player)
+    {
+        followedPlayer = player;
+    }
+}
---
+++ d01/Assets/ex00/Scripts/PlayerScript_ex00.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript_ex00 : MonoBehaviour
+{
+    public CameraController cameraController;
+    public KeyCode keycode;
+    public float jumpForce;
+    public float speed;
+
+    Rigidbody2D rb;
+    bool selected = false;
+    bool jump = false;
+    float horizontalMove;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(keycode))
+            cameraController.changeFollowedPlayer(this);
+        if (cameraController.followedPlayer == this) selected = true;
+        else selected = false;
+        if (!selected) return;
+        print(keycode);
+        if (Input.GetKeyDown(KeyCode.Space)) jump = true;
+        horizontalMove = Input.GetAxis("Horizontal");
+
+    }
+    private void FixedUpdate()
+    {
+        if (jump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+        else
+        {
+            rb.MovePosition(transform.position +
+                transform.right * horizontalMove * speed * Time.fixedDeltaTime);
+        }
+    }
+}
