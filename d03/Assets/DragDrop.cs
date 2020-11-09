288995ec42d5e422d32a42208a97e50b91594b33
Pierre Lamusse <plamusse42@gmail.com>
2020-11-09 17:39:28 +0100

    [add] - d03 - ex01

---
+++ d03/Assets/DragDrop.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour,
+    IPointerDownHandler,
+    IBeginDragHandler,
+    IEndDragHandler,
+    IDragHandler
+{
+    public gameManager gameManager;
+    public GameObject tower;
+    private RectTransform rectTransform;
+    private Vector2 initialPos;
+
+    private int towerCost;
+    SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        initialPos = rectTransform.anchoredPosition;
+    }
+
+    private void Start()
+    {
+        towerCost = tower.GetComponent<towerScript>().energy;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (towerCost > gameManager.playerEnergy)
+            spriteRenderer.color = Color.gray;
+        else
+            spriteRenderer.color = Color.white;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (towerCost >
+gameManager.playerEnergy)
+        {
+            eventData.pointerDrag = null;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(worldPos, Vector2.zero);
+        bool canPlace = false;
+        Transform emptySpot = default;
+        foreach (RaycastHit2D hit in hits)
+        {
+            Collider2D collider = hit.collider;
+            if (collider.gameObject.CompareTag("tower"))
+            {
+                canPlace = false;
+                break;
+            }
+            else if (collider.gameObject.CompareTag("empty"))
+            {
+                emptySpot = collider.gameObject.transform;
+                canPlace = true;
+            }
+        }
+        if (canPlace && towerCost <=
+            gameManager.playerEnergy)
+        {
+            Instantiate(tower, emptySpot);
+            gameManager.playerEnergy -= towerCost;
+            print("canPlace");
+        }
+        else
+            print("canNotPlace");
+        rectTransform.anchoredPosition = initialPos;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+}
---
+++ d03/Assets/Scripts/menuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class menuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ex01");
+    }
+    public void QuitGame()
+    {
+        if (Application.isPlaying)
+            EditorApplication.ExecuteMenuItem("Edit/Play");
+        else
+            Application.Quit();
+    }
+}
